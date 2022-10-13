5a6ea1475b00837dbc6a188d741aab2f1242e2ec
fogarea <shiitakedd@gmail.com>
2022-10-14 02:38:16 +0300

    feat: implement transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -15,6 +15,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function transform(arr) {
 
+
+  if(!(arr instanceof Array)) throw Error('\'arr\' parameter must be an instance of the Array!');
+  if(arr.length === 0) return [];
+    let transformCommands = {
+      '--discard-next'(acc, currentIndex, ar) {
+        if(currentIndex !== ar.length - 1){
+          acc.splice(currentIndex, 1);
+          ar.splice(currentIndex, 1);
+          ar.splice(currentIndex + 1, 1);
+        } else {
+          acc.pop();
+        }
+        return acc;
+      },
+      '--discard-prev'(acc, currentIndex) {
+        currentIndex !== 0 ? acc.length = currentIndex - 1 : acc.shift();
+        return acc;
+      },
+      '--double-next'(acc, currentIndex, ar) {
+        currentIndex !== ar.length - 1 ? acc[currentIndex] = ar[currentIndex + 1] : acc.pop();
+        return acc;
+      },
+      '--double-prev'(acc, currentIndex) {
+        if (currentIndex !== 0) {
+          acc[currentIndex] = acc[currentIndex - 1];
+        } else acc.shift();
+        return acc;
+      }
+    }
+
+    let ar = [...arr]
+
+    return ar.reduce((acc, currentValue, currentIndex) => {
+      if(typeof currentValue === 'number'){
+        acc.push(currentValue);
+        return acc;
+      } else {
+        acc.push(currentValue);
+        if(currentValue === `--discard-next` || currentValue === `--discard-prev` || currentValue === `--double-next` || currentValue === `--double-prev`){
+          acc = transformCommands[currentValue](acc, currentIndex, ar);
+        }
+        return acc;
+      }
+    }, [])
+
 }
 
 module.exports = {
