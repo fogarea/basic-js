273a8a2e77ce3a6d3055fcff2df239e8095c6c8b
fogarea <shiitakedd@gmail.com>
2022-10-16 21:43:20 +0300

    feat: implement vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(dir) {
+    this.dir = dir || dir === undefined;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    const regExp = /^[A-Z]+$/;
+    const sign = '+'
+    let cryptedMessage = '';
+    let j = 0;
+    let messageToCrypt = message.toUpperCase().split('');
+    for (const currentCharacter of messageToCrypt) {
+      regExp.test(currentCharacter) ?
+          (cryptedMessage += this.getCryptCharacter(currentCharacter, key, j, sign), j++) :
+          cryptedMessage += currentCharacter;
+    }
+    return this.dir ? cryptedMessage : cryptedMessage.split('').reverse().join('');
+  }
+  decrypt(cryptedMessage, key) {
+    if (!cryptedMessage || !key) throw new Error('Incorrect arguments!');
+    const regExp = /^[A-Z]+$/;
+    const sign = '-'
+    let message = '';
+    let j = 0;
+    let messageToDecrypt = cryptedMessage.toUpperCase().split('');
+    for (const currentCharacter of messageToDecrypt) {
+      regExp.test(currentCharacter) ?
+          (message += this.getCryptCharacter(currentCharacter, key, j, sign), j++) :
+          message += currentCharacter;
+    }
+    return this.dir ? message : message.split('').reverse().join('');
+  }
+
+  getCryptCharacter(currentCharacter, key, j, sign){
+    return String.fromCharCode((
+        (eval(`${currentCharacter.charCodeAt(0) - 65}${sign}${key[j % key.length]
+            .toUpperCase()
+            .charCodeAt() - 65}`)
+            % 26
+            + 26)
+        % 26)
+        + 65);
   }
 }
 
